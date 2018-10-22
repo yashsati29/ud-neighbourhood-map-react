582dff39d1152eb8013f0e34af1d2e5f85e6e3a9
DevS <yashsati29@gmail.com>
2018-10-23 02:39:20 +0530

    Progress: Added location's data

---
+++ src/data/locations.js
@@ -0,0 +1,83 @@
+export const locations = [
+{
+	name: 'JECRC Foundation',
+	location: {
+		address: 'Shri Ram ki Nangal via Sitapura RIICO',
+		lat: 26.782285517540508,
+		lng: 75.8242433741306
+	},
+	venueId: '4c94731df7cfa1cdf617b415'
+},
+{
+	name: 'Jaipur Exhibhition & Convention Centre',
+	location: {
+		address: 'RIICO Industrial Area Sitapura',
+		lat: 26.786759046475776,
+		lng: 75.82659278168461
+	},
+	venueId: '5495646b498e8683bef9f8ed'
+},
+{
+	name: 'Crowne Plaza Jaipur Tonk Road',
+	location: {
+		address: 'Tonk Road Sitapura Industrial Area',
+		lat: 26.779738281772666,
+		lng: 75.82785145297542
+	},
+	venueId: '537b8e8611d2cbd28ac541cf'
+},
+{
+	name: 'Mahatama Gandhi Hospital and Medical Collage',
+	location: {
+		address: 'India International School Tonk Rd',
+		lat: 26.768794416065216,
+		lng: 75.85546348778051
+	},
+	venueId: '4de5f6107d8b53987c5547a5'
+},
+{
+	name: 'Pratap Plaza,Jaipur',
+	location: {
+		address: 'F-2 Pratap Marg Sanganer',
+		lat: 26.80316485639992,
+		lng: 75.81587826043936
+	},
+	venueId: '4ec0146a7ee54e4cd2cf95f3'
+},
+{
+	name: 'Rajasthan University of Health Sciences',
+	location: {
+		address: 'Kumbha Marg Sector-18 Pratap Nagar',
+		lat: 26.806428649557468,
+		lng: 75.8238064027468
+	},
+	venueId: '4f3a1df2e4b0a4d44354a379'
+},
+{
+	name: 'Marigold Hotel',
+	location: {
+		address: 'IT-20 EPIP',
+		lat: 26.788387345603397,
+		lng: 75.83125743239772
+	},
+	venueId: '56ee7dac498eb8c8b1ac9cfd'
+},
+{
+	name: 'Narayan hrudayalaya',
+	location: {
+		address: 'Kumbha Marg Rana Sanga Marg',
+		lat: 26.795185925006212,
+		lng: 75.82552590576803
+	},
+	venueId: '4e3d08948877b00cfc41b484'
+},
+{
+	name: 'Chokhi Dhani',
+	location: {
+		address: '12 Miles Tonk Road via Vatika Marg',
+		lat: 26.766890405347645,
+		lng: 75.83608081536072
+	},
+	venueId: '4b6dab03f964a520bd842ce3'
+}
+] 
